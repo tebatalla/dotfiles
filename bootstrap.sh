1e538232a0d9def3fda9c3154c0bb7874e773f5a
Tomas Batalla <tebatalla@gmail.com>
2015-03-08 22:35:10 -0700

    Remove gem from dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_profile gitignore gitconfig pryrc aliases zshrc zshrc-e oh-my-zsh rbenv gem"    # list of files/folders to symlink in homedir
+files="bash_profile gitignore gitconfig pryrc aliases zshrc zshrc-e oh-my-zsh rbenv"    # list of files/folders to symlink in homedir
 
 ##########
 
